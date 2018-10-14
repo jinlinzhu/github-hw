5ab1cec77be0e2dba69b87ecef564a104529b5aa
Kristy Knapp <kristyknapp14@gmail.com>
2018-10-14 12:56:10 -0600

    This is my hopeful fix to the previous commit. Somehow I was using a HomeController instead of the IndexController and I think that's why my previous commit messed things up.

--- dataHW/Controllers/QueueController.cs
+++ dataHW/Controllers/QueueController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 namespace dataHW.Controllers
 {
     public class QueueController : Controller
@@ -11,46 +10,37 @@ namespace dataHW.Controllers
         //Create new Queue
         static Queue<string> myQueue = new Queue<string>();
         static string message;
-
         // GET: Queue
         public ActionResult Index()
         {
             ViewBag.Message = message;
-
+            ViewBag.MyQueue = myQueue;
             return View();
         }
-
         public ActionResult AddOne()
         {
             //add new entries to the Queue
             myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
-
             //Display current Queue entries
-            message = myQueue.Count() + " new entry added to the Queue.";
+            message = "Entries in Queue: " + myQueue.Count();
             ViewBag.Message = message;
-
-            return View("Index");
+            return RedirectToAction("Index", "Queue");
         }
-
         public ActionResult AddHugeList()
         {
             //Clear the Queue
             myQueue.Clear();
-
             //Load 2,000 items into the Queue
             for (int iCount = 0; iCount < 2000; iCount++)
             {
                 //Display "New Entry and #"
                 myQueue.Enqueue("New Entry " + (myQueue.Count + 1));
             }
-
             //display current Queue
             message = "Queue contains " + myQueue.Count() + " entries.";
             ViewBag.Message = message;
-
-            return View("Index");
+            return RedirectToAction("Index", "Queue");
         }
-
         public ActionResult Display()
         {
             //Display the current items in the Queue
@@ -60,75 +50,58 @@ namespace dataHW.Controllers
                 {
                     myQueue.Peek();
                 }
-                //ViewBag.MyQueue = myQueue;
-                
-            }
 
+            }
             else
             {
                 message = "Queue is empty";
             }
 
-            ViewBag.MyQueue = myQueue;
             ViewBag.Message = message;
-            return View("Display");
+            ViewBag.MyQueue = myQueue;
+            return RedirectToAction("Index", "Queue");
         }
-
         public ActionResult DeleteFrom()
         {
             //delete any item and inform user if it cannot delete
-
             if (myQueue.Count > 0)
             {
-
                 myQueue.Dequeue();
                 message = "An entry was deleted from the Queue.";
-                
-            }
 
+            }
             else
             {
                 message = "The Entry was not deleted because the Queue is empty.";
-                
+
             }
             //display message to the user
             ViewBag.Message = message;
-            return View("Index");
+            return RedirectToAction("Index", "Queue");
         }
-
         public ActionResult Clear()
         {
             myQueue.Clear();
             message = "Queue has been cleared.";
             ViewBag.Message = message;
-
-            return View("Index");
+            return RedirectToAction("Index", "Queue");
         }
-
         public ActionResult Search()
         {
             //stopwatch code
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-
             sw.Start();
-
             //loop to do all the work
             myQueue.ToArray();
-            
-
 
             sw.Stop();
-
             TimeSpan ts = sw.Elapsed;
-
             ViewBag.StopWatch = ts;
-
-            return View("Index");
+            return RedirectToAction("Index", "Queue");
         }
-
         public ActionResult Return()
         {
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Index");
         }
     }
 }
